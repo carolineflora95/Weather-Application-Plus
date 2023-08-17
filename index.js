67635fcbc731dfe2cdc9735d8d737a63495052f9
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2023-08-17 12:15:29 +0200

    Updated JS
    Search function and geolocation

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${latitude}&lat=${longitude}&key=${apiKey}&units=metric`;
-
 let now = new Date();
 let currentTime = document.querySelector(".time");
 let date = now.getDate();
@@ -33,12 +30,48 @@ let months = [
   "November",
   "December",
 ];
+
 let month = months[now.getMonth()];
 currentTime.innerHTML = `${hours}:${minutes}, ${day} ${date} ${month}, ${year}`;
 
-function displayTemperature(response) {
+function showTemp(response) {
+  let currentTemp = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = `${currentTemp}°C`;
+  let cityHeading = document.querySelector("#cityT");
+  cityHeading.innerHTML = response.data.name;
+}
+function searchTemp(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchCity").value;
+  let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchTemp);
+
+function showLocTemp(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let locationTemp = document.querySelector("#temperature");
+  locationTemp.innerHTML = `${temperature}°C`;
+
+  let cityHeading = document.querySelector("#cityT");
+  let cityName = document.querySelector("#searchCity").value;
+  cityHeading.innerHTML = `${cityName}`;
+  cityHeading.innerHTML = response.data.name;
+}
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+
+  let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(showLocTemp);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-axios.get(apiUrl).then(displayTemperature);
+let locationButton = document.querySelector("geoButton");
+locationButton.addEventListener("click", getCurrentPosition);
