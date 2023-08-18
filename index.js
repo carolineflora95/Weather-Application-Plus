b7aee5b8806fc6a28b0284bf165458027119e7c7
carolineflora95 <139001115+carolineflora95@users.noreply.github.com>
2023-08-18 15:26:43 +0200

    Overhaul
    Deleted buddy code, overwrote the JS file with corrected code.
    
    Added API integration for conditions, temperature and city.

--- index.js
+++ index.js
@@ -34,44 +34,24 @@ let months = [
 let month = months[now.getMonth()];
 currentTime.innerHTML = `${hours}:${minutes}, ${day} ${date} ${month}, ${year}`;
 
-function showTemp(response) {
-  let currentTemp = Math.round(response.data.main.temp);
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = `${currentTemp}°C`;
-  let cityHeading = document.querySelector("#cityT");
-  cityHeading.innerHTML = response.data.name;
-}
-function searchTemp(event) {
-  event.preventDefault();
-  let city = document.querySelector("#searchCity").value;
-  let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
-}
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchTemp);
-
-function showLocTemp(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let locationTemp = document.querySelector("#temperature");
-  locationTemp.innerHTML = `${temperature}°C`;
-
-  let cityHeading = document.querySelector("#cityT");
-  let cityName = document.querySelector("#searchCity").value;
-  cityHeading.innerHTML = `${cityName}`;
-  cityHeading.innerHTML = response.data.name;
-}
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-
-  let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
-  axios.get(apiUrl).then(showLocTemp);
-}
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showPosition);
+  let cityElement = document.querySelector("#cityT");
+  let conditionsElement = document.querySelector("#conditions");
+  let speedElement = document.querySelector("#wSpeed");
+  let humidityElement = document.querySelector("#humidity");
+  let feelElement = document.querySelector("#fLike");
+  temperatureElement.innerHTML =
+    Math.round(response.data.temperature.current) + " °C";
+  cityElement.innerHTML = response.data.city;
+  conditionsElement.innerHTML = response.data.condition.description;
+  speedElement.innerHTML = Math.round(response.data.wind.speed) + " Km/h";
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+  feelElement.innerHTML =
+    Math.round(response.data.temperature.feels_like) + "°C";
 }
 
-let locationButton = document.querySelector(".geoButton");
-locationButton.addEventListener("click", getCurrentPosition);
+let apiKey = "9a33fd779e40o2b13tb533b7a79f4beb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${"Madrid"}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
